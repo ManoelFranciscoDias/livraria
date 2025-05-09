78f5d307388b6ffcccf2c205edd6e3af900d80f0
ManoelFranciscoDias <manoel2007dias@gmail.com>
2025-05-08 23:06:52 -0300

    finalizado aula 06

--- livraria/admin.py
+++ livraria/admin.py
@@ -2,6 +2,10 @@ from django.contrib import admin
 
 from .models import Categoria
 from .models import Editora
+from .models import Autor
+from .models import Livro
 
 admin.site.register(Categoria)
 admin.site.register(Editora)
+admin.site.register(Autor)
+admin.site.register(Livro)
